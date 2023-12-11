363e47dc6877eb2f78bb564162dab704949a5cac
Crissyactress <christalriziki@gmail.com>
2023-12-11 11:18:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function submitForm() {
+            // You can add additional logic here for form submission,
+            // like sending data to a server or storing it in local storage.
+            alert('Booking Successful!');
+        }
